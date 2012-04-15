1c00ca7730adc0f992ab104e071a8e7513a39201
mlcomp <mlcomp@domU-12-31-38-00-40-16.compute-1.internal>
2012-04-15 12:15:35 -0400

    update worker with octave

--- site/lib/mlcomp/EC2Manager.rb
+++ site/lib/mlcomp/EC2Manager.rb
@@ -48,7 +48,7 @@ class EC2Manager
     @maxWaitTime, @minStartInstanceInterval, @maxIdleTime,
         = extractArgs(:args => args, :spec => [
       ['verbose', Fixnum, 0],
-      ['ami', String, 'ami-a630cbcf'], # worker5 (worker version 30)
+      ['ami', String, 'ami-262ff74f'], # worker6 (worker version 30)
       ['zone', String, 'us-east-1a'],
       ['instanceType', String, 'm1.small'],
       ['minWorkers', Fixnum, 0],
